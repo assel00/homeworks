c4ce173ac7dab0374d79f02bb792bcfd1e7f1565
Auzbayeva Assel <asel_shu.kz@mail.ru>
2023-10-07 14:19:55 +0600

    ///

--- Hw2src/App.js
+++ Hw2src/App.js
@@ -1,35 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 import withLogger from './withLogger';
 
-class App extends React.Component {
-  componentDidMount() {
-    console.log('App component has mounted.');
-  }
+function App() {
+  const [count, setCount] = useState(0);
 
-  render() {
-    const [count, setCount] = useState(0);
+  useEffect(() => {
+    document.title = `Count: ${count}`;
+  }, [count]);
 
-    useEffect(() => {
-      document.title = `Count: ${count}`;
-    }, [count]);
+  const increment = useCallback(() => {
+    setCount(count + 1);
+  }, [count]);
 
-    const increment = () => {
-      setCount(count + 1);
-    };
+  const decrement = useCallback(() => {
+    setCount(count - 1);
+  }, [count]);
 
-    const decrement = () => {
-      setCount(count - 1);
-    };
-
-    return (
-      <div>
-        <h1>Counter App</h1>
-        <p>Count: {count}</p>
-        <button onClick={increment}>Increment</button>
-        <button onClick={decrement}>Decrement</button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <h1>Counter App</h1>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
 }
 
 const AppWithLogger = withLogger(App);
